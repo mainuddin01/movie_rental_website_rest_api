e10feec108669b5cbe8120544210c34e91a923f1
unknown <freelancermainuddin@gmail.com>
2020-09-21 14:36:45 +0600

    added fawn library to perform transactional db operations using mongos two phase commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 const app = express();
 
@@ -22,6 +23,8 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 
 app.use("/api/movies", movies);
+
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 5000;
 app.listen(5000, () => {
--- routes/rentals.js
+++ routes/rentals.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const Fawn = require("fawn");
+const mongoose = require("mongoose");
 const { Rental, validate } = require("../models/rentals");
 const { Customer } = require("../models/customers");
 const { Movie } = require("../models/movies");
+
+Fawn.init(mongoose); // we are using fawn package for db transaction
 
 router.get("/", async (request, response) => {
   const rentals = await Rental.find().sort("-dateOut");
@@ -37,10 +41,33 @@ router.post("/", async (request, response) => {
     },
   });
 
-  rental = await rental.save();
+  //   rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
+  //   movie.numberInStock--;
+  //   movie.save();
 
-  response.send(rental);
+  try {
+    // we need to use Fawn.Task() to perform several operation together as a transactional unit
+    // fawn library performs mongodb's two phase commit to perform transactional tasks (basically it creates a separate collection and saves transactional tasks information in it as a document and deletes that document after the task completed successfully)
+    Fawn.Task()
+      .save("rentals", rental) // here we need to use the actual collection name in database as the first argument
+      .update(
+        "movies",
+        { _id: movie._id },
+        {
+          // here we need to use the actual collection name in database as the first argument and the query object to find the collection instance
+          $inc: {
+            numberInStock: -1,
+          },
+        }
+      )
+      // .remove() // we can use this method to upform operations as like as save() and update()
+      .run(); // finally we need to run the task
+
+    response.send(rental);
+  } catch (error) {
+    response.status(500).send("Something failed...");
+  }
 });
+
+module.exports = router;
