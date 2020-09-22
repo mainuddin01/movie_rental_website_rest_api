502f024746576adf34162ab7a245574cc1cb6e0f
unknown <freelancermainuddin@gmail.com>
2020-09-23 02:03:56 +0600

    moved all logging related codes in logging module inside startup file

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
 const config = require("config");
-require("winston-mongodb");
 const express = require("express");
-const winston = require("winston");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi); // here "joi-objectid" package returns a function and if we call that function with "Joi" class as an input then it'll return a function that can be assigned in Joi object as a method
 
 const app = express();
+
+// logging related codes import. needs to be import first before other custom module in order to logg errors if any error occours during loading of the below modules
+require("./startup/logging")();
 
 // routes
 require("./startup/routes")(app); // ./startup/routes module returns us a function and we need to pass a reference of our express app in it to use that app internally on this module
 
 // db connection
 require("./startup/db")();
-
-// added file transport to store log on file
-winston.add(winston.transports.File, { filename: "logfile.log" }); // to store logs in a file
-winston.add(winston.transports.MongoDB, { db: "mongodb://localhost/vidly" }); // to store logs in mongodb
-// winston.add(winston.transports.MongoDB, {
-//   db: "mongodb://localhost/vidly",
-//   level: "error",
-// });
-
-// to handle uncaught exception (any type of unhandled exception) in our project we can do the following
-// process.on("uncaughtException", (error) => {
-//   console.log("UNHANDLED EXCEPTION FOUND");
-//   winston.error(error.message, error);
-// });
-
-// process.on("unhandledRejection", (error) => {
-//   console.log("UNHANDLED REJECTION FOUND");
-//   winston.error(error.message, error);
-//   process.exit(1);
-// });
-
-// another way to handle uncaughtException and unhandledRejection
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (error) => {
-  throw error;
-});
 
 // throw new Error("Errrrrrrrooooooooorrrrrrr"); // simulates an unhandled exception
 
---
+++ startup/logging.js
@@ -0,0 +1,33 @@
+const winston = require("winston");
+require("winston-mongodb");
+
+module.exports = function () {
+  // added file transport to store log on file
+  winston.add(winston.transports.File, { filename: "logfile.log" }); // to store logs in a file
+  winston.add(winston.transports.MongoDB, { db: "mongodb://localhost/vidly" }); // to store logs in mongodb
+  // winston.add(winston.transports.MongoDB, {
+  //   db: "mongodb://localhost/vidly",
+  //   level: "error",
+  // });
+
+  // to handle uncaught exception (any type of unhandled exception) in our project we can do the following
+  // process.on("uncaughtException", (error) => {
+  //   console.log("UNHANDLED EXCEPTION FOUND");
+  //   winston.error(error.message, error);
+  // });
+
+  // process.on("unhandledRejection", (error) => {
+  //   console.log("UNHANDLED REJECTION FOUND");
+  //   winston.error(error.message, error);
+  //   process.exit(1);
+  // });
+
+  // another way to handle uncaughtException and unhandledRejection
+  winston.handleExceptions(
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+
+  process.on("unhandledRejection", (error) => {
+    throw error;
+  });
+};
